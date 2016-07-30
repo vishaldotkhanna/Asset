79f304cd021730db918191a5959544ea59897e38
vishaldotkhanna <vishalkhanna995@gmail.com>
2016-07-30 17:31:19 +0530

    Update update.py

--- update.py
+++ update.py
@@ -2,7 +2,7 @@ import sqlite3 as db
 from main import connect_db
 from datetime import date, datetime, timedelta
 
-print('test ' + str(date.today()))
+print('Releasing assets for ' + str(date.today()))
 conn = connect_db()
 curs =  conn.cursor()
 #curs.execute('update asset set releasedate = (?) where isreserved = 1', [str(date.today())])
